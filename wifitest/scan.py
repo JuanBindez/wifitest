221d155ae9f766268cca81bcf14860ff25ca5032
Juan Bindez <juanbindez780@gmail.com>
2024-04-21 14:58:30 -0300

    Update scan.py

--- wifitest/scan.py
+++ wifitest/scan.py
@@ -3,13 +3,29 @@ from pywifi import const
 
 
 class WifiScan():
+    """
+    Class to perform Wi-Fi network scanning and print details of the scanned networks.
+    """    
+    
     def scan_wifi_networks(self):
+        """
+        Scan for Wi-Fi networks and return the scan results.
+
+        Returns:
+            list: A list of network scan results.
+        """
         wifi = pywifi.PyWiFi()
         iface = wifi.interfaces()[0]  # obtém a primeira interface Wi-Fi disponível
         iface.scan()
         return iface.scan_results()
 
     def print_wifi_details(self, networks):
+        """
+        Print details of the scanned Wi-Fi networks.
+
+        Args:
+            networks (list): A list of network scan results.
+        """
         for network in networks:
             print("SSID:", network.ssid)
             print("BSSID:", network.bssid)
@@ -18,6 +34,15 @@ class WifiScan():
             print("----------------------------------")
 
     def translate_encryption_type(self, encryption_type):
+        """
+        Translate encryption type code to human-readable format.
+
+        Args:
+            encryption_type (int): Encryption type code.
+
+        Returns:
+            str: Human-readable encryption type.
+        """
         if encryption_type == const.AKM_TYPE_NONE:
             return "Open"
         elif encryption_type == const.AKM_TYPE_WPA:
